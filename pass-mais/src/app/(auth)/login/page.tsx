0c4fb205d799074b9393637a94a83bf58ff3ff40
guilherme <guilhermevs54@gmail.com>
2025-05-01 17:23:25 -0300

    Teste CI ECR

--- pass-mais/src/app/(auth)/login/page.tsx
+++ pass-mais/src/app/(auth)/login/page.tsx
@@ -14,7 +14,7 @@ export default function Login() {
             <div className="w-[33.75rem] mx-auto m-[100px] mb-[0px]">
 
                 <div className="w-[22.1rem] mx-auto ">
-                    <h2 className="text-2xl font-semibold mb-[24px] text-center">Seja bem-vindo de volta!</h2>
+                    <h2 className="text-2xl font-semibold mb-[24px] text-center">Seja bem-vindo de volta</h2>
                     < LoginForm />
 
                     <div className="size-12 font-regular text-center w-full line-we">
