d27d3cdcc7a0bc24bcbba849df4b6cf3ddb29e33
guilherme <guilhermevs54@gmail.com>
2025-08-24 18:21:44 -0300

    fix(auth): ajustes na página de login e configuração do NextAuth

--- pass-mais/src/app/(auth)/login/page.tsx
+++ pass-mais/src/app/(auth)/login/page.tsx
@@ -6,31 +6,26 @@ import Footer from "@/components/Footer";
 import Link from 'next/link';
 
 export default function Login() {
+  return (
+    <div className="flex h-[100vh] w-[100vw]">
+      <LoginImage />
 
-    return (
-        <div className="flex h-[100vh] w-[100vw]">
-            <LoginImage />
+      <div className="w-[33.75rem] mx-auto m-[100px] mb-[0px]">
+        <div className="w-[22.1rem] mx-auto ">
+          <h2 className="text-2xl font-semibold mb-[24px] text-center">Seja bem-vindo de volta</h2>
+          <LoginForm />
 
-            <div className="w-[33.75rem] mx-auto m-[100px] mb-[0px]">
-
-                <div className="w-[22.1rem] mx-auto ">
-                    <h2 className="text-2xl font-semibold mb-[24px] text-center">Seja bem-vindo de volta</h2>
-                    < LoginForm />
-
-                    <div className="size-12 font-regular text-center w-full line-we">
-                        <span>Ainda não tem conta?</span>
-                        <Link href="/register">
-                            <span className="text-[#007AFF]"> Crie uma conta aqui</span>
-                        </Link>
-                    </div>
-                </div>
-                <div className="mt-[350px]">
-                    <Footer />
-                </div>
-
-            </div>
-
+          <div className="size-12 font-regular text-center w-full line-we">
+            <span>Ainda não tem conta?</span>
+            <Link href="/register">
+              <span className="text-[#007AFF]"> Crie uma conta aqui</span>
+            </Link>
+          </div>
         </div>
-
-    )
+        <div className="mt-[350px]">
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
 }
