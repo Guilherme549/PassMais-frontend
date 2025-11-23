9f3e3bfb40918610a6a73a4e5db065778fbe3d7c
guilherme <guilhermevs54@gmail.com>
2025-11-23 11:57:02 -0300

    Refactor tratando erros de build

--- pass-mais/src/app/layout.tsx
+++ pass-mais/src/app/layout.tsx
@@ -1,6 +1,13 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { SessionWatcher } from "@/components/SessionWatcher";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+});
 
 export const metadata: Metadata = {
   title: {
@@ -21,12 +28,8 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-<<<<<<< HEAD
-      <body suppressHydrationWarning className="bg-gray-100">
-=======
       <body suppressHydrationWarning className={`${roboto.className} ${roboto.variable} bg-gray-100`}>
         <SessionWatcher />
->>>>>>> fbf6171 (Refactor tratando erros de build)
         {children}
       </body>
     </html>
